6fd68d4e96b435013a8af40f52f3341dae704756
Wern Ancheta <ancheta.wern@gmail.com>
2018-06-25 15:28:04 +0800

    add new sample data and corresponding images

--- src/data/pokemon.js
+++ src/data/pokemon.js
@@ -34,6 +34,139 @@ const pokemon_data = [
     name: "Meowth",
     pic: require("../img/meowth.png"),
     full_pic: require("../img/meowth.jpg")
+  },
+
+  {
+    id: 7,
+    name: "Absol",
+    pic: require("../img/absol.png"),
+    full_pic: require("../img/absol.jpg")
+  },
+  {
+    id: 8,
+    name: "Arcanine",
+    pic: require("../img/arcanine.png"),
+    full_pic: require("../img/arcanine.jpg")
+  },
+  {
+    id: 9,
+    name: "Blaziken",
+    pic: require("../img/blaziken.png"),
+    full_pic: require("../img/blaziken.jpg")
+  },
+  {
+    id: 10,
+    name: "Darkrai",
+    pic: require("../img/darkrai.png"),
+    full_pic: require("../img/darkrai.jpg")
+  },
+  {
+    id: 11,
+    name: "Deoxys",
+    pic: require("../img/deoxys.png"),
+    full_pic: require("../img/deoxys.jpg")
+  },
+  {
+    id: 12,
+    name: "Dialga",
+    pic: require("../img/dialga.png"),
+    full_pic: require("../img/dialga.jpg")
+  },
+  {
+    id: 13,
+    name: "Empoleon",
+    pic: require("../img/empoleon.png"),
+    full_pic: require("../img/empoleon.jpg")
+  },
+  {
+    id: 14,
+    name: "Entei",
+    pic: require("../img/entei.png"),
+    full_pic: require("../img/entei.jpg")
+  },
+  {
+    id: 15,
+    name: "Groudon",
+    pic: require("../img/groudon.png"),
+    full_pic: require("../img/groudon.jpg")
+  },
+  {
+    id: 16,
+    name: "Gyarados",
+    pic: require("../img/gyarados.png"),
+    full_pic: require("../img/gyarados.jpg")
+  },
+  {
+    id: 17,
+    name: "Ho-oh",
+    pic: require("../img/ho-oh.png"),
+    full_pic: require("../img/ho-oh.jpg")
+  },
+  {
+    id: 18,
+    name: "Kyurem",
+    pic: require("../img/kyurem.png"),
+    full_pic: require("../img/kyurem.jpg")
+  },
+  {
+    id: 19,
+    name: "Leafeon",
+    pic: require("../img/leafeon.png"),
+    full_pic: require("../img/leafeon.jpg")
+  },
+  {
+    id: 20,
+    name: "Lugia",
+    pic: require("../img/lugia.png"),
+    full_pic: require("../img/lugia.jpg")
+  },
+  {
+    id: 21,
+    name: "Magmortar",
+    pic: require("../img/magmortar.png"),
+    full_pic: require("../img/magmortar.jpg")
+  },
+  {
+    id: 22,
+    name: "Manectric",
+    pic: require("../img/manectric.png"),
+    full_pic: require("../img/manectric.jpg")
+  },
+  {
+    id: 23,
+    name: "Metagross",
+    pic: require("../img/metagross.png"),
+    full_pic: require("../img/metagross.jpg")
+  },
+  {
+    id: 24,
+    name: "Salamence",
+    pic: require("../img/salamence.png"),
+    full_pic: require("../img/salamence.jpg")
+  },
+  {
+    id: 25,
+    name: "Samurott",
+    pic: require("../img/samurott.png"),
+    full_pic: require("../img/samurott.jpg")
+  },
+  {
+    id: 26,
+    name: "Scyther",
+    pic: require("../img/scyther.png"),
+    full_pic: require("../img/scyther.jpg")
+  },
+  {
+    id: 27,
+    name: "Skarmory",
+    pic: require("../img/skarmory.png"),
+    full_pic: require("../img/skarmory.jpg")
+  },
+  {
+    id: 28,
+    name: "Talonflame",
+    pic: require("../img/talonflame.png"),
+    full_pic: require("../img/talonflame.jpg")
   }
 ];
 
